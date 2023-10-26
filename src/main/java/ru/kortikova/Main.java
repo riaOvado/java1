2bbce6649563d6408253041845dd470733f72676
Daria Kortikova <d4704@mail.ru>
2023-10-26 17:15:38 +0300

    first commit

---
+++ src/main/java/ru/kortikova/Main.java
@@ -0,0 +1,18 @@
+package ru.kortikova;
+
+public class Main {
+    public static void main(String[] args) {
+
+        summa(6, "arrr");
+    }
+
+    public static void summa(int a, String str){
+
+        for (int i = a; i > 0 ; i--) {
+            System.out.println(str);
+        }
+
+    }
+
+}
+
